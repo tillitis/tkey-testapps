a3f4e25bfbcfce4fafa06ec7972a09baf68f6b48
Daniel Jobson <jobson@tillitis.se>
2024-12-30 14:35:40 +0100

    Follow REUSE specification
    - Add REUSE configuration
    - Add compliant license in LICENSES/
    - Add copyright SPDX tags
    - Run REUSE Compliance check in CI

--- apps/nx/main.c
+++ apps/nx/main.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2023-2024 - Tillitis AB
+// SPDX-FileCopyrightText: 2023 Tillitis AB <tillitis.se>
 // SPDX-License-Identifier: BSD-2-Clause
 
 // Test of the execution monitor. Should blink green 5 times, then the
