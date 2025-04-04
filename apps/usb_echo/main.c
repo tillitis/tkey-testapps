1ffdf7e496bfbcd5bd0223915dc9a8b0410520c5
Mikael Ågren <mikael@tillitis.se>
2025-04-04 10:01:18 +0200

    usb_echo: Use new tkey-libs io api
    - Enable USB endpoints
    - Use readselect, read and write functions

--- apps/usb_echo/main.c
+++ apps/usb_echo/main.c
@@ -3,14 +3,15 @@
 
 #include <tkey/assert.h>
 #include <tkey/led.h>
-#include <tkey/proto.h>
+#include <tkey/io.h>
 #include <tkey/tk1_mem.h>
 
-#define MODE_TKEYCTRL 0x20
-#define MODE_CDC 0x40
-#define MODE_HID 0x80
-
 #define FORWARD_TO_DEBUG_HID 0
+#if FORWARD_TO_DEBUG_HID
+#define ENABLED_EPS (IO_CDC | IO_FIDO | IO_DEBUG)
+#else
+#define ENABLED_EPS (IO_CDC | IO_FIDO)
+#endif
 
 // clang-format off
 static volatile uint32_t *cpu_mon_ctrl  = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_CTRL;
@@ -22,9 +23,11 @@ static volatile uint32_t *app_size      = (volatile uint32_t *) TK1_MMIO_TK1_APP
 
 int main(void)
 {
-	uint8_t mode = 0;
+	enum ioend ep = 0;
 	uint8_t len = 0;
 	uint8_t data[256];
+
+	config_endpoints(ENABLED_EPS);
 
 	// Use Execution Monitor on RAM after app
 	*cpu_mon_first = *app_addr + *app_size;
@@ -34,36 +37,18 @@ int main(void)
 	led_set(LED_BLUE);
 
 	for (;;) {
-		mode = readbyte();
-		len = readbyte();
 
-		for (int i = 0; i < len; i++) {
-			data[i] = readbyte();
 		}
 
-		switch (mode) {
-		case MODE_CDC:
-			break;
-		case MODE_HID:
-			break;
-		default:
-			assert(1 == 2);
-		}
+		readselect(IO_CDC | IO_FIDO, &ep, &len);
+		read(ep, data, sizeof(data), len);
 
 		// Echo data
-		writebyte(mode);
-		writebyte(len);
-		for (int i = 0; i < len; i++) {
-			writebyte(data[i]);
-		}
+		write(ep, data, len);
 
 #if FORWARD_TO_DEBUG_HID
 		// Write data to debug end point
-		writebyte(MODE_TKEYCTRL);
-		writebyte(len);
-		for (int i = 0; i < len; i++) {
-			writebyte(data[i]);
-		}
+		write(IO_DEBUG, data, len);
 #endif
 	}
 }
